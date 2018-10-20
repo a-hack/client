a298d9074df0769e6f1129347ba2bb01b830a60c
bearium <jacob_martin@rogers.com>
2018-10-20 13:38:09 -0400

    APi connection

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import enigma from "enigma.js";
 import schema from "enigma.js/schemas/12.20.0.json";
-import qlikConnect from "../utils/QlickConnection";
+import qlikConnect from "../utils/QlikConnection";
 
 
 class App extends Component {
