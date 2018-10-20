47ff82265331bf785a29629640a7dc6beb86bbc8
Patrick Gingras <ging0044@algonquinlive.com>
2018-10-20 12:45:20 -0400

    add router to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import HomepageLayout from "./layouts/HomepageLayout";
+import Dashboard from "./layouts/Dashboard";
 
-class App extends Component {
-	render() {
-		return (
-			<HomepageLayout/>
-		);
-	}
-}
+import "./App.css";
 
-export default App;
+export default () => (
+	<BrowserRouter>
+		<Route exact path="/">
+			<HomepageLayout />
+		</Route>
+	</BrowserRouter>
+);
