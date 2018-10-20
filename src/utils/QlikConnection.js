c5abcad5d079b0d24c0f0a947ba201d6778ebbfc
bearium <jacob_martin@rogers.com>
2018-10-20 15:53:26 -0400

    connection to Qix

--- src/utils/QlikConnection.js
+++ src/utils/QlikConnection.js
@@ -10,23 +10,51 @@ class QlikConnection {
 			createSocket: url => new WebSocket(url),
 		});
 
-		session.on("traffic:sent", data => console.log("sent:", data));
-		session.on("traffic:received", data => console.log("received:", data));
+		const properties = {
+			qInfo: {
+				qType: 'visualization',
+				qId: '',
+			},
+			qHyperCubeDef: {
+				qDimensions: [
+					{
+						qDef: {qFieldDefs: ['Goal ID']},
+					},
+				],
+				qMeasures: [
+					{
+						qDef: {qDef: '=Sum(Value)'},
+					},
+				],
+				qInitialDataFetch: [
+					{
+						qHeight: 5,
+						qWidth: 2,
+					},
+				],
+			},
+		};
 
-		// open the socket and eventually receive the QIX global API, and then close
-		// the session:
-		session.open().then((global) => {
-			global.openDoc("0b0fc6d5-05ce-44d7-95aa-80d0680b3559").then((doc) => {
-				doc.getObject("").then(api => {
-					api.getLayout().then(() => {
-						api.getProperties().then((props) => {
-							console.log(props);
-						});
-					});
-				});
+
+		// Open the session and create a session document:
+		this.q = session.open()
+			.then((global) => {
+				return global.openDoc("0b0fc6d5-05ce-44d7-95aa-80d0680b3559");
+			})
+			// Create a generic object with a hypercube definition containing one dimension and one measure
+			.then((doc) => doc.createSessionObject(properties));
+		this.q.then((object) => object.getLayout().then(layout => {
+			return layout.qHyperCube.qDataPages;
+		}))
+		// Get hypercube layout
+			.catch((error) => {
+				console.log('Session: Failed to open socket:', error);
 			});
-		})
-			.catch(err => console.log("Something went wrong :(", err));
+	}
+
+	getQ() {
+		return this.q;
 	}
 }
+
 export default QlikConnection;
