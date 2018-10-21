73789a6277c0baf4d7feeb6a4495193eb41edac5
macdja38 <jakeincanada@icloud.com>
2018-10-21 10:10:25 -0400

    Make map display selected country

--- src/widgets/WorldMap.js
+++ src/widgets/WorldMap.js
@@ -1,300 +1,30 @@
-import React, { Component } from "react"
-import {
-	ComposableMap,
-	ZoomableGroup,
-	Geographies,
-	Geography,
-	Markers,
-	Marker,
-} from "react-simple-maps"
-import { scaleLinear } from "d3-scale"
-import data from "./world-50m-with-population"
+import React, { Component } from "react";
+import { ComposableMap, Geographies, Geography, Marker, Markers, ZoomableGroup, } from "react-simple-maps";
+import { scaleLinear } from "d3-scale";
+import worldData from "./world-50m-with-population";
 import { WidgetProps } from "../consts";
-import {VictoryPie} from "victory"
+import { VictoryPie } from "victory";
 
 const wrapperStyles = {
 	width: "100%",
 	maxWidth: 980,
 	margin: "0 auto",
 };
-export const cantons = [
-	{
-		id: 25,
-		canton: "Zurich",
-		coordinates: [ 30.6356, 47.3595 ],
-		languages: [
-			{ name: "German", value: 83.1 },
-			{ name: "French", value: 3 },
-			{ name: "Italian", value: 5.9 },
-			{ name: "Romansh", value: 0.4 }
-		]
-	}, {
-		id: 6,
-		canton: "Bern",
-		coordinates: [ 190.7081, 46.7989 ],
-		languages: [
-			{ name: "German", value: 84.7 },
-			{ name: "French", value: 10.3 },
-			{ name: "Italian", value: 3.1 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 12,
-		canton: "Luzern",
-		coordinates: [ 8.1662, 47.0796 ],
-		languages: [
-			{ name: "German", value: 89.9 },
-			{ name: "French", value: 1.5 },
-			{ name: "Italian", value: 3.2 },
-			{ name: "Romansh", value: 0.2 }
-		]
-	}, {
-		id: 22,
-		canton: "Uri",
-		coordinates: [ 8.6025, 46.7739 ],
-		languages: [
-			{ name: "German", value: 93.6 },
-			{ name: "French", value: 1.1 },
-			{ name: "Italian", value: 2.1 },
-			{ name: "Romansh", value: null }
-		]
-	}, {
-		id: 18,
-		canton: "Schwyz",
-		coordinates: [ 8.7904, 47.0724 ],
-		languages: [
-			{ name: "German", value: 89.6 },
-			{ name: "French", value: 1.7 },
-			{ name: "Italian", value: 2.8 },
-			{ name: "Romansh", value: 0.5 }
-		]
-	}, {
-		id: 15,
-		canton: "Obwalden",
-		coordinates: [ 8.18, 46.8779 ],
-		languages: [
-			{ name: "German", value: 91.8 },
-			{ name: "French", value: 1.3 },
-			{ name: "Italian", value: 1.8 },
-			{ name: "Romansh", value: null }
-		]
-	}, {
-		id: 14,
-		canton: "Nidwalden",
-		coordinates: [ 8.3850, 46.95 ],
-		languages: [
-			{ name: "German", value: 92.1 },
-			{ name: "French", value: 2.1 },
-			{ name: "Italian", value: 2.5 },
-			{ name: "Romansh", value: null }
-		]
-	}, {
-		id: 9,
-		canton: "Glarus",
-		coordinates: [ 9.0672, 47.0404 ],
-		languages: [
-			{ name: "German", value: 86.8 },
-			{ name: "French", value: 1.5 },
-			{ name: "Italian", value: 5.9 },
-			{ name: "Romansh", value: 0.5 }
-		]
-	}, {
-		id: 26,
-		canton: "Zug",
-		coordinates: [ 8.52, 47.18 ],
-		languages: [
-			{ name: "German", value: 82.6 },
-			{ name: "French", value: 3.5 },
-			{ name: "Italian", value: 3.8 },
-			{ name: "Romansh", value: 0.5 }
-		]
-	}, {
-		id: 7,
-		canton: "Fribourg",
-		coordinates: [ 7.1173, 46.6817 ],
-		languages: [
-			{ name: "German", value: 27.8 },
-			{ name: "French", value: 68.1 },
-			{ name: "Italian", value: 2.5 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 19,
-		canton: "Solothurn",
-		coordinates: [ 7.6388, 47.3321 ],
-		languages: [
-			{ name: "German", value: 89.6 },
-			{ name: "French", value: 3 },
-			{ name: "Italian", value: 4.3 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 5,
-		canton: "Basel-Stadt",
-		coordinates: [ 7.5928, 47.5619 ],
-		languages: [
-			{ name: "German", value: 78.6 },
-			{ name: "French", value: 5.2 },
-			{ name: "Italian", value: 6.3 },
-			{ name: "Romansh", value: 0.2 }
-		]
-	}, {
-		id: 4,
-		canton: "Basel-Land",
-		coordinates: [ 7.7644, 47.4418 ],
-		languages: [
-			{ name: "German", value: 88 },
-			{ name: "French", value: 3.3 },
-			{ name: "Italian", value: 5.5 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 17,
-		canton: "Schaffhausen",
-		coordinates: [ 8.5680, 47.7009 ],
-		languages: [
-			{ name: "German", value: 86.9 },
-			{ name: "French", value: 1.9 },
-			{ name: "Italian", value: 4.3 },
-			{ name: "Romansh", value: 0.3 }
-		]
-	}, {
-		id: 2,
-		canton: "Appenzell Ausserrhoden",
-		coordinates: [ 9.3001, 47.3665 ],
-		languages: [
-			{ name: "German", value: 93.8 },
-			{ name: "French", value: 1.1 },
-			{ name: "Italian", value: 2.5 },
-			{ name: "Romansh", value: 0.5 }
-		]
-	}, {
-		id: 3,
-		canton: "Appenzell Inherrhoden",
-		coordinates: [ 9.4317, 47.3162 ],
-		languages: [
-			{ name: "German", value: 95 },
-			{ name: "French", value: null },
-			{ name: "Italian", value: 1.4 },
-			{ name: "Romansh", value: null }
-		]
-	}, {
-		id: 16,
-		canton: "St. Gallen",
-		coordinates: [ 9.3504, 47.1456 ],
-		languages: [
-			{ name: "German", value: 88.8 },
-			{ name: "French", value: 1.2 },
-			{ name: "Italian", value: 3.5 },
-			{ name: "Romansh", value: 0.3 }
-		]
-	}, {
-		id: 10,
-		canton: "Graubunden",
-		coordinates: [ 9.5780, 46.6570 ],
-		languages: [
-			{ name: "German", value: 74.7 },
-			{ name: "French", value: 1.5 },
-			{ name: "Italian", value: 13.3 },
-			{ name: "Romansh", value: 15.9 }
-		]
-	}, {
-		id: 1,
-		canton: "Aargau",
-		coordinates: [ 8.2554, 47.3877 ],
-		languages: [
-			{ name: "German", value: 87.8 },
-			{ name: "French", value: 2.1 },
-			{ name: "Italian", value: 4.9 },
-			{ name: "Romansh", value: 0.2 }
-		]
-	}, {
-		id: 20,
-		canton: "Thurgau",
-		coordinates: [ 9.0557, 47.6038 ],
-		languages: [
-			{ name: "German", value: 89.5 },
-			{ name: "French", value: 1.3 },
-			{ name: "Italian", value: 4.1 },
-			{ name: "Romansh", value: 0.2 }
-		]
-	}, {
-		id: 21,
-		canton: "Ticino",
-		coordinates: [ 8.8005, 46.3317 ],
-		languages: [
-			{ name: "German", value: 10.9 },
-			{ name: "French", value: 4.9 },
-			{ name: "Italian", value: 88.5 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 24,
-		canton: "Vaud",
-		coordinates: [ 6.5368, 46.5613 ],
-		languages: [
-			{ name: "German", value: 6.3 },
-			{ name: "French", value: 83.3 },
-			{ name: "Italian", value: 5.3 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 23,
-		canton: "Valais",
-		coordinates: [ 7.5449, 46.1905 ],
-		languages: [
-			{ name: "German", value: 25.5 },
-			{ name: "French", value: 67.7 },
-			{ name: "Italian", value: 4.2 },
-			{ name: "Romansh", value: 0.1 }
-		]
-	}, {
-		id: 13,
-		canton: "Neuchatel",
-		coordinates: [ 6.7716, 46.9752 ],
-		languages: [
-			{ name: "German", value: 5.2 },
-			{ name: "French", value: 87.8 },
-			{ name: "Italian", value: 5.8 },
-			{ name: "Romansh", value: null }
-		]
-	}, {
-		id: 8,
-		canton: "Geneva",
-		coordinates: [ 6.1217, 46.2180 ],
-		languages: [
-			{ name: "German", value: 5.1 },
-			{ name: "French", value: 81 },
-			{ name: "Italian", value: 6.7 },
-			{ name: "Romansh", value: 0 }
-		]
-	}, {
-		id: 11,
-		canton: "Jura",
-		coordinates: [ 7.1431, 47.3444 ],
-		languages: [
-			{ name: "German", value: 7.7 },
-			{ name: "French", value: 90.4 },
-			{ name: "Italian", value: 3.1 },
-			{ name: "Romansh", value: null }
-		]
-	}
-];
 
 const popScale = scaleLinear()
-	.domain([0,100000000,1400000000])
-	.range(["#CFD8DC","#607D8B","#37474F"])
+	.domain([0, 100000000, 1400000000])
+	.range(["#CFD8DC", "#607D8B", "#37474F"])
 
 class BasicMap extends Component {
 	static propTypes = WidgetProps;
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {};
 	}
 
 	componentDidMount() {
-		const {app} = this.props;
+		const { app } = this.props;
 
 		app.createCube({
 			qDimensions: [{
@@ -311,37 +41,42 @@ class BasicMap extends Component {
 				qWidth: 3,
 			}],
 		}, (reply) => {
-			const mydata=[];
-			mydata.push({
-				id: 11,
-				canton: "Jura",
-				coordinates: [ 7.1431, 47.3444 ],
-				languages: [
-					{ name: "German", value: 7.7 },
-					{ name: "French", value: 90.4 },
-					{ name: "Italian", value: 3.1 },
-					{ name: "Romansh", value: null }
-				]
-			});
 			console.log("chaka", reply.qHyperCube.qDataPages[0].qMatrix);
-			reply.qHyperCube.qDataPages[0].qMatrix.forEach(function(entry) {
-				entry.forEach(function(entry) {
-
-				})
-			});
-			console.log(mydata)
+			console.log("baka", worldData.objects.units.geometries.map(entry => entry.properties.sovereignt));
+			const data = reply.qHyperCube.qDataPages[0].qMatrix;
+			const dataMap = reply.qHyperCube.qDataPages[0].qMatrix.reduce((acc, country) => {
+				console.log("country", country[0].qText);
+				acc[country[0].qText] = true;
+				return acc;
+			}, {});
+			this.setState({ data, dataMap });
 		});
 
 	}
 
+	handleClick(data) {
 
+	}
 
-
-	handleClick(data){
-		console.log(data)
+	stateDataHasCountry(country) {
+		if (this.state.data) {
+			return this.state.dataMap.hasOwnProperty(country);
+		}
+		return true;
 	}
 
 	render() {
+		console.log(worldData, this.state.dataMap);
+
+		let afterData = Object.assign({}, worldData, {
+			objects: {
+				units: Object.assign({}, worldData.objects.units, {
+					geometries: worldData.objects.units.geometries.filter(entry => {
+						return this.stateDataHasCountry(entry.properties.sovereignt);
+					}),
+				}),
+			},
+		});
 		return (
 			<div style={wrapperStyles}>
 				<ComposableMap
@@ -353,8 +88,8 @@ class BasicMap extends Component {
 						height: "auto",
 					}}
 				>
-					<ZoomableGroup center={[0,20]}>
-						<Geographies geography={data}>
+					<ZoomableGroup center={[0, 20]}>
+						<Geographies geography={afterData}>
 							{(geographies, projection) =>
 								geographies.map((geography, i) =>
 									<Geography
@@ -386,47 +121,6 @@ class BasicMap extends Component {
 									/>
 								)}
 						</Geographies>
-						<Markers>
-							{cantons.map((canton, i) => (
-								<Marker
-									key={ `canton-${canton.id}` }
-									marker={ canton }
-									style={{
-										default: {
-											outline: "none",
-										},
-										hover: {
-											outline: "none",
-										},
-										pressed: {
-											outline: "none",
-										},
-									}}
-								>
-									<g transform="translate(-15,-15)">
-										<circle cx={20} cy={20} r={21} fill="transparent" stroke="#607D8B" />
-										<circle cx={20} cy={20} r={9} fill="transparent" stroke="#607D8B" />
-										<VictoryPie
-											standalone={ false }
-											width={ 40 }
-											height={ 40 }
-											padding={ 0 }
-											innerRadius={ 10 }
-											style={{
-												labels: { fill: "transparent" },
-												data: { stroke: "#ECEFF1" },
-											}}
-											data={[
-												{ x: null, y: canton.languages[0].value, fill: "#A7F432" },
-												{ x: null, y: canton.languages[1].value, fill: "#00BCD4" },
-												{ x: null, y: canton.languages[2].value, fill: "#5DADEC" },
-												{ x: null, y: canton.languages[3].value, fill: "#8BC34A" },
-											]}
-										/>
-									</g>
-								</Marker>
-							))}
-						</Markers>
 					</ZoomableGroup>
 				</ComposableMap>
 			</div>
